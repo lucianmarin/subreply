e68d8b759d922c740114dbb0ad2e71617bf793b2
Lucian Marin <marin.lucian@gmail.com>
2020-06-13 00:57:04 +0300

    renamed socket

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
 import multiprocessing
 from project.settings import DEBUG
 
-bind = "127.0.0.1:8000" if DEBUG else "unix:dub.socket"
-pidfile = "dub.pid"
+bind = "127.0.0.1:8000" if DEBUG else "unix:sub.socket"
+pidfile = "sub.pid"
 workers = 1 if DEBUG else multiprocessing.cpu_count() * 2 + 1
 threads = 1 if not DEBUG else multiprocessing.cpu_count() * 2 + 1
 worker_class = "gthread" if DEBUG else "meinheld.gmeinheld.MeinheldWorker"
