559ddc74288abb3561b2bca2a51fe58e0985cda9
Lucian Marin <marin.lucian@gmail.com>
2020-06-18 22:02:08 +0300

    fix gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,6 @@ from project.settings import DEBUG
 
 bind = "127.0.0.1:8000" if DEBUG else "unix:sub.socket"
 pidfile = "sub.pid"
-workers = 1 if DEBUG else multiprocessing.cpu_count() * 2 + 1
-threads = 1 if not DEBUG else multiprocessing.cpu_count() * 2 + 1
-worker_class = "gthread" if DEBUG else "meinheld.gmeinheld.MeinheldWorker"
+threads = multiprocessing.cpu_count() * 2 + 1
+worker_class = "gthread"
 reload = DEBUG
