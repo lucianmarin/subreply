4fb05b8813d042ee31c5a6e30c5205af17f269b2
Lucian Marin <marin.lucian@gmail.com>
2021-02-04 05:56:00 +0200

    update dark theme

--- app/jinja.py
+++ app/jinja.py
@@ -18,7 +18,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 23
+env.globals['v'] = 24
 
 
 def render(page, **kwargs):
