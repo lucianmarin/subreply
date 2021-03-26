a40241bf2c764bffa51d41aa78acf539eef98daf
Lucian Marin <marin.lucian@gmail.com>
2021-03-27 00:52:01 +0200

    flat white

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 31
+env.globals['v'] = 32
 
 
 def render(page, **kwargs):
