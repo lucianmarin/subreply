4444367d20bc2a49480a1908d8e1e2a484cab6b8
Lucian Marin <marin.lucian@gmail.com>
2021-04-04 01:41:45 +0300

    improved submit style

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 33
+env.globals['v'] = 34
 
 
 def render(page, **kwargs):
