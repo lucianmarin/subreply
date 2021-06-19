53fadb80143481d28d205064398857ea091171ea
Lucian Marin <marin.lucian@gmail.com>
2021-06-19 04:48:45 +0300

    fix extra profile links

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 43
+env.globals['v'] = 44
 
 
 def render(page, **kwargs):
