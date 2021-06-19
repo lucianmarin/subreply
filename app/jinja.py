91d92c4cfa13926fa278f30401592179da062e7a
Lucian Marin <marin.lucian@gmail.com>
2021-06-19 04:41:46 +0300

    one line extra profile

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 42
+env.globals['v'] = 43
 
 
 def render(page, **kwargs):
