b604ae08da838108ba7a9cdbc26a53b2f430a367
Lucian Marin <marin.lucian@gmail.com>
2021-07-18 01:12:10 +0300

    fixed trending

--- app/resources.py
+++ app/resources.py
@@ -520,12 +520,12 @@ class DiscoverResource:
 
 
 class TrendingResource:
-    sample = 20
+    sample = 19
 
     def fetch_entries(self, req):
         sampling = Comment.objects.filter(parent=None).annotate(
-            replies=Count('kids'), max_id=Max('descendants')
-        ).exclude(replies=0).order_by('-max_id').values('id')[:self.sample]
+            replies=Count('kids')
+        ).exclude(replies=0).order_by('-id').values('id')[:self.sample]
         entries = Comments.filter(
             id__in=sampling
         ).order_by('-replies', '-id').prefetch_related(PFR)
