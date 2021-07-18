83ecccc49e6ef3192a27883ed3bf10e691a7266c
Lucian Marin <marin.lucian@gmail.com>
2021-07-18 23:50:49 +0300

    fixed sidebar

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 45
+env.globals['v'] = 46
 
 
 def render(page, **kwargs):
