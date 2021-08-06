aa2513e5f8fe00025c7b379710c5c136eaf68c1d
Lucian Marin <marin.lucian@gmail.com>
2021-08-06 15:09:24 +0300

    white svg fill

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 50
+env.globals['v'] = 51
 
 
 def render(page, **kwargs):
