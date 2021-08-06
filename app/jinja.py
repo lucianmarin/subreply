9885e3f5896dffc3b35207234267dd11e8d95e42
Lucian Marin <marin.lucian@gmail.com>
2021-08-06 15:14:12 +0300

    define svg path and circle

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 51
+env.globals['v'] = 52
 
 
 def render(page, **kwargs):
