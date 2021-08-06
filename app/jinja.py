95ae9c05cad5ef8846931f3553b51fac2c67938d
Lucian Marin <marin.lucian@gmail.com>
2021-08-06 15:16:16 +0300

    gray path and circle

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 52
+env.globals['v'] = 53
 
 
 def render(page, **kwargs):
