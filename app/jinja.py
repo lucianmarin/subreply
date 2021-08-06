b6236b5bf5c84f1e0201132524f295460fe657ad
Lucian Marin <marin.lucian@gmail.com>
2021-08-06 15:05:36 +0300

    black svg stroke

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 48
+env.globals['v'] = 49
 
 
 def render(page, **kwargs):
