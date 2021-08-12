753627e6731735ba70554812604a205fcf81b01c
Lucian Marin <marin.lucian@gmail.com>
2021-08-12 05:29:13 +0300

    use css root

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 57
+env.globals['v'] = 58
 
 
 def render(page, **kwargs):
