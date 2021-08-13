bcf88c5eb76c3b3732e0a796b286d438ef3d5e8e
Lucian Marin <marin.lucian@gmail.com>
2021-08-14 02:01:05 +0300

    improved tabs

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 60
+env.globals['v'] = 61
 
 
 def render(page, **kwargs):
