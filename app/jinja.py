b33098c6293cabb496d99aca8332f8e6e236c8f7
Lucian Marin <marin.lucian@gmail.com>
2021-08-15 03:30:51 +0300

    improved accent color

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 63
+env.globals['v'] = 64
 
 
 def render(page, **kwargs):
