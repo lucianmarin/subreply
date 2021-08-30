e0b4e3307cc97f5343b87517a52847ed1045f75d
Lucian Marin <marin.lucian@gmail.com>
2021-08-31 01:48:45 +0300

    fixed loader and spacing

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 66
+env.globals['v'] = 67
 
 
 def render(page, **kwargs):
