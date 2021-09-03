e160bb461fecf15b7cd2975d1e108be9c77898fb
Lucian Marin <marin.lucian@gmail.com>
2021-09-03 21:49:03 +0300

    fixed padding

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 68
+env.globals['v'] = 1
 
 
 def render(page, **kwargs):
