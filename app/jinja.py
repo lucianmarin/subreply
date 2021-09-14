e6cd70e318761a26b6646e6fa4919e36a406740c
Lucian Marin <marin.lucian@gmail.com>
2021-09-14 15:54:07 +0300

    set back opacity

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 12
+env.globals['v'] = 13
 
 
 def render(page, **kwargs):
