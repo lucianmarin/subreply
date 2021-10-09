9d3ba68609512df15f53bcbc9e29ced69b012ed7
Lucian Marin <marin.lucian@gmail.com>
2021-10-09 19:46:00 +0300

    revert and improve about page

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 18
+env.globals['v'] = 19
 
 
 def render(page, **kwargs):
