722b1c1acaab10d3f40aef15be174f0687a8d483
Lucian Marin <marin.lucian@gmail.com>
2021-10-10 01:18:55 +0300

    align form inputs

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 20
+env.globals['v'] = 21
 
 
 def render(page, **kwargs):
