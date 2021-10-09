6679ee2cd61aba4c909ed8a64b174dbd53b4e793
Lucian Marin <marin.lucian@gmail.com>
2021-10-09 19:01:18 +0300

    full size emoji logo

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 17
+env.globals['v'] = 18
 
 
 def render(page, **kwargs):
