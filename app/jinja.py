95110dd3b4d04555f022bd4c354848a9ab48ac9d
Lucian Marin <marin.lucian@gmail.com>
2021-10-09 20:07:36 +0300

    fixed logo font size

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 19
+env.globals['v'] = 20
 
 
 def render(page, **kwargs):
