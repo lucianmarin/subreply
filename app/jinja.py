f308a34fe9d5992b2a252f886a9411596e6563ca
Lucian Marin <marin.lucian@gmail.com>
2021-10-11 21:52:07 +0300

    clean up requirements

--- app/jinja.py
+++ app/jinja.py
@@ -1,7 +1,6 @@
 from urllib.parse import quote_plus
 
 from jinja2 import Environment, FileSystemBytecodeCache, FileSystemLoader
-from num2words import num2words
 from emoji import emojize
 
 from app.filters import age, city, parser, shortdate, superscript
@@ -15,7 +14,6 @@ env.auto_reload = DEBUG
 env.filters['age'] = age
 env.filters['city'] = city
 env.filters['emojize'] = emojize
-env.filters['num2words'] = num2words
 env.filters['parser'] = parser
 env.filters['quote'] = quote_plus
 env.filters['shortdate'] = shortdate
