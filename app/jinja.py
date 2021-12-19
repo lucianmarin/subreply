719b169a46f015f7b83fc6f93e4b8f3c044e013a
Lucian Marin <marin.lucian@gmail.com>
2021-12-19 13:31:52 +0200

    implement inline replies

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 36
+env.globals['v'] = 137
 
 
 def render(page, **kwargs):
