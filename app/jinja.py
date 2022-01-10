2507fa5cd1dbd9310e4440191d1eb0b6e6467c53
Lucian Marin <marin.lucian@gmail.com>
2022-01-10 21:33:56 +0200

    spaced title pages

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 147
+env.globals['v'] = 148
 
 
 def render(page, **kwargs):
