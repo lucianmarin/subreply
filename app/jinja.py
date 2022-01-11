d8ed3add4aeccaefc42de281f88862f4aa16a67f
Lucian Marin <marin.lucian@gmail.com>
2022-01-11 22:52:27 +0200

    mini tablet mode

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 149
+env.globals['v'] = 150
 
 
 def render(page, **kwargs):
