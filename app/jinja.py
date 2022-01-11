9a62b243bfb567156ebcbaa0b3e260a1a7112119
Lucian Marin <marin.lucian@gmail.com>
2022-01-11 23:30:24 +0200

    sharp border

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 150
+env.globals['v'] = 151
 
 
 def render(page, **kwargs):
