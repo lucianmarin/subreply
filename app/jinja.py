910c06a767c5bcfd6e30d3989fa269092d5323a1
Lucian Marin <marin.lucian@gmail.com>
2022-01-12 15:36:34 +0200

    add search icon

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 153
+env.globals['v'] = 154
 
 
 def render(page, **kwargs):
