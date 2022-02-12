59371dc10c6ffaa8016e3eadfb0a54d87a5612a1
Lucian Marin <marin.lucian@gmail.com>
2022-02-12 15:02:27 +0200

    align to bottom on mobile

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 160
+env.globals['v'] = 161
 
 
 def render(page, **kwargs):
