cd9e76dd74c38cf9a23a8e22527115b4243a7a5e
Lucian Marin <marin.lucian@gmail.com>
2022-02-12 03:22:49 +0200

    new layout fixes

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 158
+env.globals['v'] = 159
 
 
 def render(page, **kwargs):
