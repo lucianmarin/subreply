24e4fee9e19ee04c94663dccb1ccd52d00eab138
Lucian Marin <lucian.marin@2checkout.com>
2022-02-24 15:04:37 +0200

    updated filters

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 166
+env.globals['v'] = 167
 
 
 def render(page, **kwargs):
