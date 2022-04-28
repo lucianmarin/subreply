e931734ef5c5f7b0da68f6b5caee5cac3076d261
Lucian Marin <marin.lucian@gmail.com>
2022-04-28 15:21:58 +0300

    removed empty links

--- app/jinja.py
+++ app/jinja.py
@@ -21,7 +21,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 176
+env.globals['v'] = 177
 
 
 def render(page, **kwargs):
