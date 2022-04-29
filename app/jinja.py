d356a13d85896470d950ba5bf4bc504755acda03
Lucian Marin <marin.lucian@gmail.com>
2022-04-29 18:00:19 +0300

    hide desc when read

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 182
+env.globals['v'] = 183
 
 
 def render(page, **kwargs):
