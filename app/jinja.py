78bf7159bac080edebb3e78dbf08fa7e64a2db52
Lucian Marin <marin.lucian@gmail.com>
2022-04-29 17:25:08 +0300

    mark as read

--- app/jinja.py
+++ app/jinja.py
@@ -22,7 +22,7 @@ env.filters['shortdate'] = shortdate
 env.filters['superscript'] = superscript
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 180
+env.globals['v'] = 181
 
 
 def render(page, **kwargs):
