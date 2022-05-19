ad508661a25ed9993a814af69dc02ead1a7a1809
Lucian Marin <marin.lucian@gmail.com>
2022-05-19 09:31:28 +0300

    revert smoke bg

--- app/jinja.py
+++ app/jinja.py
@@ -26,7 +26,7 @@ env.filters['shorten'] = lambda text, width: shorten(text, width, placeholder=".
 env.filters['keywords'] = lambda e: ", ".join(e[1:-1].split("_"))
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 197
+env.globals['v'] = 201
 
 
 def render(page, **kwargs):
