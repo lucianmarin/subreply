9bb2ae85003f5efd22f3f033886bd8600f961dec
Lucian Marin <marin.lucian@gmail.com>
2022-11-10 19:19:58 +0200

    improved read

--- app/jinja.py
+++ app/jinja.py
@@ -28,7 +28,7 @@ env.filters['shorten'] = lambda txt, w: shorten(txt, w, placeholder="...")
 env.filters['keywords'] = lambda emo: ", ".join(emo[1:-1].split("_"))
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 210
+env.globals['v'] = 211
 
 
 def render(page, **kwargs):
