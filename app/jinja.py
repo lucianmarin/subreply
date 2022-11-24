30180479af687bffc4bd267f5f82d79dd909c393
Lucian Marin <marin.lucian@gmail.com>
2022-11-24 12:26:16 +0200

    fixed border spacing

--- app/jinja.py
+++ app/jinja.py
@@ -26,7 +26,7 @@ env.filters['shorten'] = lambda txt, w: shorten(txt, w, placeholder="...")
 env.filters['keywords'] = lambda emo: ", ".join(emo[1:-1].split("_"))
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 216
+env.globals['v'] = 217
 
 
 def render(page, **kwargs):
