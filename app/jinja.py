9d86b783591a88ba8c09895b77c208163b53e850
Lucian Marin <marin.lucian@gmail.com>
2022-11-26 02:15:25 +0200

    fixed side items

--- app/jinja.py
+++ app/jinja.py
@@ -26,7 +26,7 @@ env.filters['shorten'] = lambda txt, w: shorten(txt, w, placeholder="...")
 env.filters['keywords'] = lambda emo: ", ".join(emo[1:-1].split("_"))
 
 env.globals['brand'] = "Subreply"
-env.globals['v'] = 225
+env.globals['v'] = 226
 
 
 def render(page, **kwargs):
