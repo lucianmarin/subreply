2de3a19c832a2c2718b230f62ac63b8f4711f132
Lucian Marin <marin.lucian@gmail.com>
2023-05-05 22:03:17 +0300

    improved deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 PREVIOUS=$PWD
 cd ~/subreply/
 git pull
-python3 -m pip install -U -r requirements.txt
+python3 -m pip install -U -r requirements.txt --break-system-packages
 python3 manage.py migrate
 pkill -HUP -F sub.pid
 cd $PREVIOUS
