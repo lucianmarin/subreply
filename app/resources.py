047c4442155267d3f8202ab8094c22facfb8a8a1
Lucian Marin <marin.lucian@gmail.com>
2023-05-12 22:24:27 +0300

    revert discover

--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,6 @@
 from cgi import FieldStorage
 
-from django.db.models import Count, Prefetch, Q
+from django.db.models import Count, Prefetch, Q, Max
 from emails import Message
 from emails.template import JinjaTemplate
 from emoji import demojize, get_emoji_unicode_dict
@@ -476,10 +476,12 @@ class DiscoverResource:
         return query
 
     def fetch_entries(self, terms):
-        entries = Comments
         if terms:
-            entries = Comments.filter(self.build_query(terms))
-        return entries.order_by('-id').prefetch_related(PFR, PPFR)
+            f = self.build_query(terms)
+        else:
+            last_ids = User.objects.annotate(last=Max('comments')).values('last')
+            f = Q(id__in=last_ids)
+        return Comments.filter(f).order_by('-id').prefetch_related(PFR, PPFR)
 
     @before(auth_user)
     def on_get(self, req, resp):
