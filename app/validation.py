a136d7210390a16dd55f89394b85f961d6d82ca4
Lucian Marin <marin.lucian@gmail.com>
2024-06-18 16:34:02 +0300

    fixed self link error message

--- app/validation.py
+++ app/validation.py
@@ -55,7 +55,7 @@ def valid_content(value, user, limit=640):
         elif link == value.lower():
             return "Share more than a link"
         elif link.startswith(('http://subreply.com', 'https://subreply.com')):
-            return "Share a @mention, a #sub or a reply #id"
+            return "Share a mention, a sub or a reply id"
     elif mentions:
         mention = mentions[0]
         if user and mention == user.username:
