0826643eb81e6e92081d4aa336263fb90e290595
Lucian Marin <marin.lucian@gmail.com>
2025-05-21 19:05:57 +0300

    venv deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 PREVIOUS=$PWD
 cd ~/subreply/
 git pull
-python3 -m pip install -U -r requirements.txt
-python3 manage.py migrate
+venv/bin/pip install -U -r requirements.txt
+venv/bin/python3 manage.py migrate
 pkill -HUP -F sub.pid
 cd $PREVIOUS
