0b08993052adaeeafe42f5d29ec5bd2ee73f2483
Lucian Marin <marin.lucian@gmail.com>
2026-01-18 17:22:12 +0200

    configure for performance

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,6 @@
-from multiprocessing import cpu_count
-
 from project.settings import DEBUG
 
 bind = "127.0.0.1:8000" if DEBUG else "unix:sub.socket"
 pidfile = "sub.pid"
-threads = 1 if DEBUG else cpu_count() * 2 + 1
-worker_class = "gthread"
+workers = 1 if DEBUG else 4
 reload = DEBUG
