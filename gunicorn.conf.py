2cb3bd1dc38abdbeb340795ce90bd95158c8b5de
Lucian Marin <marin.lucian@gmail.com>
2026-02-15 15:36:06 +0200

    update number of workers

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,5 +2,5 @@ from project.settings import DEBUG
 
 bind = "127.0.0.1:8000" if DEBUG else "unix:sub.socket"
 pidfile = "sub.pid"
-workers = 1 if DEBUG else 4
+workers = 1 if DEBUG else 3
 reload = DEBUG
